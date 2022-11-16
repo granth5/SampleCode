89098d56df70e33b875d99a003246a087d357ef7
Grant Hickey <granth341@hotmail.com>
2022-11-16 10:52:17 -0500

    Added DataTableExtensions, TypedSimpleSettingsHelper

---
+++ DataTableExtensions.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace GHHSoftware.Common.ExtensionMethods
+{
+  public static class DataTableExtensions
+    {
+        public static string GetStringSafe(this DataRow row, string columnName)
+        {
+            return row[columnName] != DBNull.Value ? row[columnName].ToString() : string.Empty;
+        }
+
+        public static T GetValueSafe<T>(this DataRow row, string columnName)
+        {
+            return row[columnName] != DBNull.Value ? (T)row[columnName] : default(T);
+        }
+
+        public static T GetValueSafe<T>(this DataRow row, string columnName, T defaultOverride)
+        {
+            return row[columnName] != DBNull.Value ? (T)row[columnName] : defaultOverride;
+        }
+
+        public static DateTime? GetDateTimeOrNull(this DataRow row, string columnName)
+        {
+            if (row[columnName] == DBNull.Value)
+            {
+                return null;
+            }
+            else if (Convert.ToDateTime(row[columnName]).ToString("MM/dd/yyyy") == "01/01/1901")
+            {
+                return null;
+            }
+            else
+            {
+                return Convert.ToDateTime(row[columnName]);
+            }
+        }
+
+        public static T GetValueSafe<T>(this SqlDataReader reader, string columnName, T defaultOverride)
+        {
+            return reader[columnName] != DBNull.Value ? (T)reader[columnName] : defaultOverride;
+        }
+    }
+}
---
+++ GHHTypedSimpleSettingsHelper.cs
@@ -0,0 +1,65 @@
+﻿using SoftCircuits.CsvParser;
+using System;
+using System.IO;
+
+namespace GHHSoftware.Common.Helpers
+{
+  public class GHHTypedSimpleSettingsHelper
+  {
+    public string ErrorMessage { get; }
+    private string _errorMessage;
+
+    public T LoadSettings<T>(string settingsFile = "") where T : class, new()
+    {
+      object retVal = null;
+      _errorMessage = string.Empty;
+
+      if (File.Exists(settingsFile))
+      {
+        try
+        {
+          using (CsvReader<T> rdr = new CsvReader<T>(settingsFile))
+          {
+            rdr.ReadHeaders(true);
+
+            retVal = rdr.Read();
+          }
+        }
+        catch (Exception ex)
+        {
+          _errorMessage = $"Exception in LoadSettings(): {ex.Message}";
+        }
+      }
+      else
+      {
+        _errorMessage = $"Could not locate settings file: {settingsFile}";
+      }
+
+      return retVal as T;
+    }
+
+    public bool SaveSettings<T>(string settingsFile, T settingsClass) where T : class, new() 
+    {
+      var retVal = false;
+      _errorMessage = string.Empty;
+
+      try
+      {
+        using (CsvWriter<T> wtr = new CsvWriter<T>(settingsFile))
+        {
+          wtr.WriteHeaders();
+          wtr.Write(settingsClass);
+
+          // if we got here, all's well!
+          retVal = true;
+        }
+      }
+      catch (Exception e)
+      {
+        _errorMessage = $"Exception in SaveSettings<T>(): {e.Message}";
+      }
+
+      return retVal;
+    }
+  }
+}
